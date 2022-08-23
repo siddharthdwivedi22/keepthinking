fd70e2f83ccebeaf7d248e333d7bafa36fe3d6ca
Sid Dwivedi <s.dwivedi@wwiis.co.uk>
2022-08-24 00:19:27 +0100

    Updates

--- hierarchies.php
+++ hierarchies.php
@@ -14,21 +14,6 @@ $locations = array(
     11 => array("City2")
 );
 
-// Multidimensional array
-$superheroes = array(
-    "spider-man" => array(
-        "name" => "Peter Parker",
-        "email" => "peterparker@mail.com",
-    ),
-    "super-man" => array(
-        "name" => "Clark Kent",
-        "email" => "clarkkent@mail.com",
-    ),
-    "iron-man" => array(
-        "name" => "Harry Potter",
-        "email" => "harrypotter@mail.com",
-    )
-);
  //echo $locations[3][1];
 // Printing all the keys and values one by one
 $keys = array_keys($locations);
